0a27efaf20d157b66b6053e20e187d181651bcdc
suzhentxt <chaunguyentotrinh2006@gmail.com>
2023-07-09 23:16:39 +0700

    second commit

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-$(document).ready(function(){
-	$(window).scroll(function () {
-			if ($(this).scrollTop() > 50) {
-				$('#back-to-top').fadeIn();
-			} else {
-				$('#back-to-top').fadeOut();
-			}
-		});
-		// scroll body to 0px on click
-		$('#back-to-top').click(function () {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 400);
-			return false;
-		});
-});
+$(document).ready(function() {
+	// Xử lý khi form đăng nhập được submit
+	$("#loginForm").submit(function(e) {
+	  e.preventDefault();
+	  // Thực hiện xử lý đăng nhập
+	  // ...
+	});
+  
+	// Xử lý khi form đăng ký được submit
+	$("#signupForm").submit(function(e) {
+	  e.preventDefault();
+	  // Thực hiện xử lý đăng ký
+	  // ...
+	});
+  
+	// Xử lý khi modal đăng ký được đóng
+	$("#signupModal").on("hidden.bs.modal", function() {
+	  $(this).detach().appendTo("body");
+	});
+  
+	// Xử lý khi người dùng cuộn trang
+	$(window).scroll(function() {
+	  if ($(this).scrollTop() > 50) {
+		$('#back-to-top').fadeIn();
+	  } else {
+		$('#back-to-top').fadeOut();
+	  }
+	});
+  
+	// Cuộn trang về đầu khi nhấp vào nút "back-to-top"
+	$('#back-to-top').click(function() {
+	  $('body,html').animate({
+		scrollTop: 0
+	  }, 400);
+	  return false;
+	});
+  });
+  
